5f6e9db3f041f0902755c84b1bf1a94376c43e88
Andrei <titov680andrei@gmail.com>
2024-08-20 17:39:43 +0400

    Added order detail to admin panel

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,7 @@ import datetime
 
 from django.contrib import admin
 from django.http import HttpResponse
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 
 from orders.models import Order, OrderItem
@@ -46,6 +47,11 @@ def export_to_csv(model_admin, request, queryset):
 export_to_csv.short_description = 'Export to CSV'
 
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -65,6 +71,7 @@ class OrderAdmin(admin.ModelAdmin):
         order_payment,
         'created',
         'updated',
+        order_detail
     ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render, redirect, get_object_or_404
 
 from cart.cart import Cart
 from orders.forms import OrderCreateForm
-from orders.models import OrderItem
+from orders.models import OrderItem, Order
 from .tasks import order_created
 
 
@@ -27,3 +28,11 @@ def order_create(request):
     else:
         form = OrderCreateForm()
         return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
